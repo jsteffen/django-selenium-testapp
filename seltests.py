fe4adc76bc0d42495ff0699679f80d4dc0bbb97e
Roman Prokofyev <roman.prokofyev@gmail.com>
2011-09-25 13:43:19 +0400

    bugfix for django 1.2 compatibility

--- seltests.py
+++ seltests.py
@@ -6,4 +6,4 @@ class MyTestCase(SeleniumTestCase):
     def test_home(self):
         self.open_url(reverse('home'))
         self.failUnless(self.is_text_present('Test Page'))
-        self.aseertEquals(self.get_title(), 'Home')
+        self.assertEquals(self.get_title(), 'Home')
--- urls.py
+++ urls.py
@@ -1,16 +1,5 @@
-from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.base import TemplateView
+from django.conf.urls.defaults import patterns, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('django.views.generic.simple',
+    url(r'^', 'direct_to_template', {'template': 'home.html'}, name='home'),
 )
